62d50b7b9584d1af1128e7170bba17e79c542a27
Ivan Castillo <total180training@gmail.com>
2021-08-04 03:47:49 -0400

    rendering first random deck for deck component

--- src/Components/Deck.js
+++ src/Components/Deck.js
@@ -1,26 +1,42 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import Card from './Card'
+
 
 
 export default function Deck() {
 
-    const [ pokemon, setPokemon ] = useState([])
-    const [ deck, setDeck ] = useState([])
-    const [ count, setCount ] = useState(0)
+    const [ deck1, setDeck1 ] = useState([])
+    const [ deck2, setDeck2 ] = useState([])
     
     const randomNum = () => {
         return Math.floor(Math.random() * 151)
     }
-    console.log(randomNum())
+    
+    const getDeck = async () => {
+        const arr = []
+        for (let i = 1; i <= 7; i++) {
+            await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNum()}`)
+            .then((res) => {
+                arr.push(res.data)
+            })
+        }
+        setDeck1(arr)
+    }
 
     useEffect(() => {
-        axios.get(` https://pokeapi.co/api/v2/pokemon/?limit=20`)
-        .then(res => console.log(res))
-    })
+        getDeck()
+    }, [])
 
     return (
         <div>
             <h2>Deck Component</h2>
+            <section className='Deck1'>
+                {deck1.length === 7  ?  deck1.map(pokemon => {return <Card key={pokemon.name} pokemon={pokemon}/>})  :  <h2>Loading...</h2>}
+            </section>
+            <section className='Deck2'>
+
+            </section>
         </div>
     )
 }
