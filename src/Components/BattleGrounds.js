ac5e3e8c7a5c7674db6057e7589b04ab13d8c988
Ivan Castillo <total180training@gmail.com>
2021-10-24 23:21:39 -0400

    Refactored Attack system for using and queuing Moves. Replaced Attack button with Move buttons. Added PP and remaining_pp to Move

--- src/Components/BattleGrounds.js
+++ src/Components/BattleGrounds.js
@@ -1,23 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import BattleCard from './BattleCard'
 import { capitalize } from '../Helper/capitalize'
-
-// Two Pokemon are battling each other side by side
-    // When one pokemon has lost, flow goes to the next pair of pokemon to battle
-// There is one attack button in the center
-// The pokemon with the highest speed attacks first
-    // The attack will be based on:    Player 1  -  Player 2
-    // remaining_hp = remaining_hp - ((atk * 3) - (def * 2))
-// handleClick() is wrapped in setTimeOut function
 
 
 export default function BattleGrounds({ round, pokemonOne, pokemonTwo }) {
 
     const [pokemon1, setPokemon1] = useState(pokemonOne)
     const [pokemon2, setPokemon2] = useState(pokemonTwo)
+    const [player1, setPlayer1] = useState({
+        ready: false,
+        move: null
+    })
+    const [player2, setPlayer2] = useState({
+        ready: false,
+        move: null
+    })
     const [narration, setNarration] = useState('')
     
-
     const randomDamage = () => {
         let min = 7
         let max = 13
@@ -38,9 +37,6 @@ export default function BattleGrounds({ round, pokemonOne, pokemonTwo }) {
                 remaining_hp: pokemon2.remaining_hp - totalDamage
             }
         })
-        setNarration('')
-        console.log('Random Damage: ', randomDamage())
-        console.log('Total damage: ', totalDamage)
     }
     const pokemon2Attack = () => {
         let atkOutput = pokemon2.atk
@@ -51,23 +47,43 @@ export default function BattleGrounds({ round, pokemonOne, pokemonTwo }) {
                 remaining_hp: pokemon1.remaining_hp - totalDamage
             }
         })
-        console.log('Random Damage: ', randomDamage())
-        console.log('Total damage: ', totalDamage)
     }
-
-    const handleClick = () => {
-        let pokeAcc1 = pokemon1.spd * randomSpeed()
-        let pokeAcc2 = pokemon2.spd * randomSpeed()
-        if (pokeAcc1 > pokeAcc2) {
+    
+    const executeAttacks = () => {
+        let pokeSpd1 = pokemon1.spd * randomSpeed()
+        let pokeSpd2 = pokemon2.spd * randomSpeed()
+        if (pokeSpd1 > pokeSpd2) {
             console.log('Pokemon 1 is faster', randomSpeed())
             pokemon1Attack()
             pokemon2Attack()
+            setPlayer1({ ready: false, move: null })
+            setPlayer2({ ready: false, move: null })
         } else {
             console.log('pokemon2 is faster', randomSpeed())
             pokemon2Attack()
             pokemon1Attack()
+            setPlayer1({ ready: false, move: null })
+            setPlayer2({ ready: false, move: null })
         }
     }
+    const setMove1 = (move) => {
+        setPlayer1({
+            ready: true,
+            move: move
+        })
+    }
+    const setMove2 = (move) => {
+        setPlayer2({
+            ready: true,
+            move: move
+        })
+    }
+
+    useEffect(() => {
+        if (player1.ready && player2.ready) {
+            executeAttacks()
+        }
+    }, [player1, player2])
 
 
     return (
@@ -77,13 +93,13 @@ export default function BattleGrounds({ round, pokemonOne, pokemonTwo }) {
                 <div className='PlayerDiv'>
                     <BattleCard pokemon={pokemon1} />
                     <span>Remaining HP: {pokemon1.remaining_hp}</span>
-                    <button>{capitalize(pokemon1.move1.name)} &nbsp; PP: {pokemon1.move1.remaining_pp}/{pokemon1.move1.pp}</button>
+                    <button onClick={() => setMove1(pokemon1.move1)}>{capitalize(pokemon1.move1.name)} &nbsp; PP: {pokemon1.move1.remaining_pp}/{pokemon1.move1.pp}</button>
                 </div>
                 <h2>VS</h2>
                 <div className='PlayerDiv'>
                     <BattleCard pokemon={pokemon2} />
                     <span>Remaining HP: {pokemon2.remaining_hp}</span>
-                    <button>{capitalize(pokemon2.move1.name)} &nbsp; PP: {pokemon2.move1.remaining_pp}/{pokemon2.move1.pp}</button>
+                    <button onClick={() => setMove2(pokemon2.move1)}>{capitalize(pokemon2.move1.name)} &nbsp; PP: {pokemon2.move1.remaining_pp}/{pokemon2.move1.pp}</button>
                 </div>
             </div>
             {/* <button onClick={handleClick}>Attack!</button> */}
@@ -100,4 +116,11 @@ export default function BattleGrounds({ round, pokemonOne, pokemonTwo }) {
 }
 
 // Each player has a button to choose a move
-    // 
+// Both players choose a move
+    // This sets both players to be ready
+    // setMove()
+// When both players are ready, both moves are executed
+    // useEffect()
+        // executeAttacks()
+            // The fastest pokemon attacks first, and then the second Pokemon attacks
+        // Both player objects are reset
