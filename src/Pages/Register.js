90965be92f7c106672aedb465be9b0d1b5bb5292
Ivan Castillo <total180training@gmail.com>
2022-12-04 05:53:47 -0500

    Register - added form and some local validation for input

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -6,7 +6,12 @@ import './Register.css'
 
 export default function Register() {
 
-    const [user, setUser] = useState({})
+    const [user, setUser] = useState({
+        username: "",
+        email: "",
+        password: ""
+    })
+    const [passwordConfirm, setPasswordConfirm] = useState("")
     
 {/*
     This page will handle all of the user account information.
@@ -20,12 +25,59 @@ export default function Register() {
     MyAccount
 */}
 
+    function handleChange(e) {
+        setUser({...user, [e.target.name]: e.target.value})
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+    }
+
+    function doPasswordsMatch() {
+        if (passwordConfirm !== user.password.slice(0, passwordConfirm.length)) return false
+
+        return true
+    }
+
 
     return (
-        <div className='User'>
+        <div className='Register' onSubmit={handleSubmit}>
             <h1>Register Page</h1>
             <form action="">
-                <label htmlFor="">What is your user name?</label><input name="userName" type="text" />
+                <label htmlFor="username">Full Name</label>
+                <input 
+                    name="username" type="text" 
+                    placeholder="John Doe" 
+                    onChange={handleChange}
+                    required 
+                /><br />
+                
+                <label htmlFor="email">E-mail</label>
+                <input 
+                    name="email" type="email" 
+                    placeholder="johndoe@email.com" 
+                    onChange={handleChange}
+                    required 
+                /><br />
+                
+                <label htmlFor="password">Password</label>
+                <input name="password" type="password" 
+                    minLength="6" title="Must be 6 characters long" 
+                    onChange={handleChange}
+                    required 
+                /><br />
+                
+                <label htmlFor="confirm-password">Confirm Password</label>
+                <input 
+                    name="confirm-password" type="password" 
+                    minLength="6" title="Must be same as password" 
+                    onChange={(e) => setPasswordConfirm(e.target.value)}
+                    required 
+                />
+                <label style={{visibility: doPasswordsMatch() ? "hidden" : "visible", color: "red"}}>Password does not match</label>
+                <br />
+                
+                <input type="submit" value="Submit" />
             </form>
         </div>
     )
