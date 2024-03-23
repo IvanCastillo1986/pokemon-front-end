39d072e929b967acf4a0fa804d94ee34ea5331c0
IvanCastillo1986 <total180training@gmail.com>
2024-03-23 06:13:43 -0400

    refactored <Navbar /> to provide sign out functionality and added some readability

--- src/Layout/Navbar.js
+++ src/Layout/Navbar.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 import { UserContext } from '../UserContext'
+import { auth } from '../firebase'
+import { signOut } from 'firebase/auth'
+
 import pokeball from '../Images/pokeball.png'
 import pokeball_icon from '../Images/pokeball_icon.png'
 import pokeball_icon_open from '../Images/pokeball_icon_open.png'
@@ -21,37 +24,37 @@ Account
     Sign Out
 */
 
-const unregisteredMenuItems = [
-    {
-        linkName: "Log In",
-        url: '/login'
-    },
-    {
-        linkName: 'Register',
-        url: '/register'
-    },
-    {
-        linkName: 'Account',
-        url: '/my-account'
-    },
-
-]
-const registeredMenuItems = [
-    {
-        linkName: 'My Account',
-        url: '/account' 
-    },
-    {
-        linkName: "Log Out",
-        url: '/logout'
-    }
-]
+// const unregisteredMenuItems = [
+//     {
+//         linkName: 'Log In',
+//         url: '/login'
+//     },
+//     {
+//         linkName: 'Register',
+//         url: '/register'
+//     },
+//     {
+//         linkName: 'Account',
+//         url: '/my-account'
+//     },
+// ]
+// const registeredMenuItems = [
+//     {
+//         linkName: 'My Account',
+//         url: '/my-account' 
+//     },
+//     {
+//         linkName: 'Log Out',
+//         url: '/logout'
+//     }
+// ]
 
 export default function Navbar() {
 
-    const {user} = useContext(UserContext)
+    const {user, setUser} = useContext(UserContext)
+    // console.log(user.currentUser)
 
-    const [showOverlay, setShowOverlay] = useState(false)
+    const [showMenu, setShowMenu] = useState(false)
     const [signedIn, setSignedIn] = useState(false)
 
     // when window size changes, this gets triggered 
@@ -59,7 +62,24 @@ export default function Navbar() {
 
     const history = useHistory()
 
+    const handleAccountClick = () => {
+        // Add menuComponent when clicked
+        setShowMenu(!showMenu)
+    }
+
     
+    const handleSignOut = () => {
+        signOut(auth)
+        .then(() => {
+            setUser({})
+            sessionStorage.clear()
+
+            history.push("/logout")
+        })
+        .catch(err => console.log('Error signing out:', err))
+    }
+    
+
     const handleResize = () => {
         setWindowWidth(window.innerWidth)
     }
@@ -71,14 +91,13 @@ export default function Navbar() {
         return () => {
             window.removeEventListener('resize', handleResize)
         }
-
     }, [window.innerWidth])
     
 
     return (
         <nav className="Navbar">
             
-            <div className="overlay" onClick={() => setShowOverlay(!showOverlay)} style={{"display" : showOverlay ? "block" : "none"}}></div>
+            <div className="overlay" onClick={() => setShowMenu(false)} style={{"display" : showMenu ? "block" : "none"}}></div>
             
             <div><Link to="/"><img src={pokeball} style={{height: "100px"}} alt="pokeball" /></Link></div>
             
@@ -88,35 +107,28 @@ export default function Navbar() {
                 <Link to="/cards">Cards</Link>
                 <Link to="/pokedex">Pokedex</Link>
                 <Link to="/play">Play</Link>
-
-                <div className='account-div' onClick={() => setShowOverlay(!showOverlay)}>
-                    <div className='account'>
-                        <span>Account</span>
-                        <img src={showOverlay ? pokeball_icon_open : pokeball_icon} alt="pokeball closed" />
-                    </div>
-                    {showOverlay && signedIn &&
-                        <ul>
-                        {registeredMenuItems.map((menuItem) => {
-                            return (
-                            <li key={menuItem.url.slice(1)} onClick={() => history.push(menuItem.url)}>
-                                {menuItem.linkName}
-                            </li>
-                            )
-                        })}
-                        </ul>
+                <div className='account'>
+                    <span  onClick={handleAccountClick}>Account</span>
+                    <img src={showMenu ? pokeball_icon_open : pokeball_icon} alt="pokeball closed" />
+                    { showMenu &&
+                    <div className='dropdown-div'>
+                        
+                        {user.currentUser &&
+                            <ul>
+                                <li to='/logout' onClick={handleSignOut}>Logout</li>
+                                <Link to='/my-account'>My Account</Link>
+                            </ul>
+                        }
+                        
+                        {!user.currentUser &&
+                            <ul>
+                                <Link to='/login'>Login</Link>
+                                <Link to='/register'>Register</Link>
+                            </ul>
+                        }
+                    </div> // closes 'dropdown-div'
                     }
-                    {showOverlay && !signedIn &&
-                        <ul>
-                        {unregisteredMenuItems.map((menuItem) => {
-                            return (
-                            <li key={menuItem.url.slice(1)} onClick={() => history.push(menuItem.url)}>
-                                {menuItem.linkName}
-                            </li>
-                            )
-                        })}
-                        </ul>
-                    }
-                </div>
+                </div> {/* closes 'account' div */}
             </>
             :
             <Hamburger />
